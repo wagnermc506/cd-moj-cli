61238d6b94c907efb006a261e1dc0349ce3a5014
wagnermc506 <wagnermc506@gmail.com>
2020-04-08 18:17:53 -0300

    Contest Submit implemented

--- bin/moj.js
+++ bin/moj.js
@@ -3,6 +3,10 @@
 const getInput = require('../src/utils/inputHandle').getInput;
 const register = require('../src/utils/contestHandle').registerContest;
 const {saveContestFile, loadAnswerFile} = require('../src/utils/fileHandle');
+const submit = require('../src/utils/requests').submit;
+
+const fs = require('fs');
+//const path = require('path');
 
 [,, ...args] = process.argv; 
 
@@ -21,7 +25,23 @@ if(args.includes("init")) {
 }
 
 else if(args[0] == "submit") {
-    if(args[1] == null) return console.log("No path");
-    const filename = loadAnswerFile(process.cwd() + '/' + args[1]);
+    // if(args[1] == null) return console.log("No path");
+    //const filename = loadAnswerFile(process.cwd() + '/' + args[1]);
+    //filename = path.parse(filename);
+    const filename = process.cwd() + '/' + args[1];
     
+    const obj = {
+        contestName: "silistaopublicocompleto20192apc",
+        username: "wagnermc506",
+        hash: "73d08870c689ea80812eb67d057176f7"
+    }
+
+    let teste = fs.readFileSync(filename);
+
+    // console.log(teste);
+    // console.log(teste.toString());
+
+    submit(obj, teste, filename);
+
+    //submit(obj, filename).then().catch((e) => {console.log(e)})
 }
--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,4 +1,5 @@
 const restler = require('restler');
+const superagent = require('superagent');
 
 function getLoginInfo(obj) {
     return new Promise( resolve => {
@@ -27,19 +28,33 @@ function getContestPage() {
     })
 }
 
-function submit() {
-    return new Promise( resolve => {
-        restler.post(`'https://moj.naquadah.com.br/cgi-bin/submite.sh/${contestName}`, {
-            multipart: true,
-            headers: {
-                cookie: `simpleTabsCookie=simpleTabsCookie_5_0; login=${username}; hash=${hash}`
-            },
-            data: {
-                problem: problemValue,
-                filename: restler.file(path, filename, null, null, null)
-            }
-        })
-    })
+// function submit(obj, path, teste) {
+//     return new Promise( resolve => {
+//         restler.post(`'https://moj.naquadah.com.br/cgi-bin/submete.sh/${obj.contestName}`, {
+//             multipart: true,
+//             headers: {
+//                 cookie: `simpleTabsCookie=simpleTabsCookie_5_0; login=${obj.username}; hash=${obj.hash}`
+//             },
+//             data: {
+//                 problem: "0",
+//                 filename: restler.file(path, null, 30000)
+//             }
+//         }).on("complete", () => {
+//             resolve();
+//         })
+//     })
+// }
+
+function submit(obj, filebuf, filename) {
+    superagent.post(`https://moj.naquadah.com.br/cgi-bin/submete.sh/${obj.contestName}`)
+        //.set("user-agent", "node/superagent")
+        .set("Cookie", `simpleTabsCookie=simpleTabsCookie_5_0; login=${obj.username}; hash=${obj.hash}`)
+        .set("Accept", "*/*")
+        .field("problem", "0")
+        .attach("filename", filebuf, filename)
+        .then(() => {
+            console.log("ok")
+        }).catch((e) => {console.log(e)})
 }
 
 module.exports = {getLoginInfo, submit}
