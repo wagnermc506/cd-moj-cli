80d1a2ecbb8c1c6cfd888190674edea615235d0a
wagnermc506 <wagnermc506@gmail.com>
2020-04-13 11:30:20 -0300

    Update of the submit feature

--- bin/moj.js
+++ bin/moj.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 
-const getInput = require('../lib/inputHandle').getInput;
-const register = require('../lib/utils/contestHandle').registerContest;
-const {saveContestFile, loadAnswerFile} = require('../lib/fileHandle');
-const submit = require('../lib/requests').submit;
+const {getInput} = require('../lib/inputHandle');
+const {registerContest, verifyHash} = require('../lib/contestHandle');
+const {saveContestFile, loadContestFile, loadAnswerFile} = require('../lib/fileHandle');
+const {submit} = require('../lib/requests');
 
-const fs = require('fs');
+// const fs = require('fs');
 //const path = require('path');
 
 [,, ...args] = process.argv; 
 
-if(args.includes("init")) {
+if(args[0] == "init") {
     let input = getInput();
     input.then(
         (result) => {
-            let contestData = register(result);
+            let contestData = registerContest(result);
             contestData.then((result) => {
                 saveContestFile(result);
             })
@@ -25,17 +25,17 @@ if(args.includes("init")) {
 }
 
 else if(args[0] == "submit") {
-    // if(args[1] == null) return console.log("No path");
-    //const filename = loadAnswerFile(process.cwd() + '/' + args[1]);
-    //filename = path.parse(filename);
-    const filename = process.cwd() + '/' + args[1];
-
-    let teste = fs.readFileSync(filename);
-
-    // console.log(teste);
-    // console.log(teste.toString());
-
-    submit(obj, teste, filename);
-    console.log("first");
-    //submit(obj, filename).then().catch((e) => {console.log(e)})
+    if(args[1] == null) return console.log("No file selected");
+    let obj = loadContestFile();
+    
+    verifyHash(obj)
+    .then((result) => {
+        if(result) {
+            saveContestFile(result);
+            obj = result;
+        }
+        const filename = process.cwd() + '/' + args[1];
+        submit(obj, filename);
+    })
+    .catch((e) => {console.log(e)});
 }
